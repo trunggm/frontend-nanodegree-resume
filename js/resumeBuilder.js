4aedf48a75956f8d16653b0e370b7424f46c5ab4
trunggm <trung.gm.vn@gmail.com>
2016-08-29 17:58:15 +0700

    add console.log

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,7 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
  $("#main").append("Giang Minh Trung");
+
+ var awesomeThoughts = "I am Giang Minh Trung and I am AWESOME!"
+
+ console.log(awesomeThoughts);
