1435a209715301b8c516985bbf377c2454b2ddbe
trunggm <trung.gm.vn@gmail.com>
2016-08-29 18:03:15 +0700

    a example of replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,8 +2,16 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- $("#main").append("Giang Minh Trung");
+ //$("#main").append("Giang Minh Trung");
 
- var awesomeThoughts = "I am Giang Minh Trung and I am AWESOME!"
+ //var awesomeThoughts = "I am Giang Minh Trung and I am AWESOME!"
 
- console.log(awesomeThoughts);
+ //console.log(awesomeThoughts);
+
+var email = "trung.gm.vn@yahoo.com"
+
+var gmail = email.replace("yahoo", "gmail");
+
+console.log(email);
+
+console.log(gmail);
