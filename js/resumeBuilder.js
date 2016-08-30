5cfdd7bd8cec87e1d0b762deb53d76b0ad6bfd67
trunggm <trung.gm.vn@gmail.com>
2016-08-30 09:22:20 +0700

    complete quiz format data, build a resume header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,10 +8,21 @@ This is empty on purpose! Your code to build the resume will go here.
 
  //console.log(awesomeThoughts);
 
-var email = "trung.gm.vn@yahoo.com"
+//var email = "trung.gm.vn@yahoo.com"
 
-var gmail = email.replace("yahoo", "gmail");
+//var gmail = email.replace("yahoo", "gmail");
 
-console.log(email);
+//console.log(email);
 
-console.log(gmail);
+//console.log(gmail);
+var name = "Giang Minh Trung";
+
+var role = "front-end developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+
+$("#header").append(formattedRole);
