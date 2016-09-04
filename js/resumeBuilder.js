e80b2696e23136198253d9922b64a18693ca7712
trunggm <trung.gm.vn@gmail.com>
2016-09-04 23:57:20 +0700

    project: add 4 object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,138 +2,89 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- //$("#main").append("Giang Minh Trung");
-
- //var awesomeThoughts = "I am Giang Minh Trung and I am AWESOME!"
-
- //console.log(awesomeThoughts);
-
-//var email = "trung.gm.vn@yahoo.com"
-
-//var gmail = email.replace("yahoo", "gmail");
-
-//console.log(email);
-
-//console.log(gmail);
-
-var name = "Giang Minh Trung";
-
-var role = "front-end developer";
-
-//var formattedName = HTMLheaderName.replace("%data%", name);
-
-//var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-//$("#header").prepend(formattedRole);
-
-//$("#header").prepend(formattedName);
-
-// array length
-var array = ["trunggm", 23, {name: "trung", lastname: "giang"}];
-
-console.log(array.length);
-
-// create bio object
+/* create  4 JS object bio, education, work, project*/
+/* 1. bio */
 var bio = {
             name: "Giang Minh Trung",
-            role: "Front-End Developer",
-            contact_info: {
+            role: "Developer",
+            contacts: {
+              moblie: "84 168 501 5121",
               email: "trung.gm.vn@gmail.com",
-              mobile: "84-168-5015-121",
               github: "trunggm",
-              facebook: "trung.gm.vn"
+              location: "HaNoi, VietNam"
             },
-            picture_url: "images/me.jpg",
-            wellcom_msg: "Well come to Homepage!",
-            skills: ["Math", "Python", "Machine Learning", "Data Science"]
-          }
-// add a city
+            welcomeMessage: "Well come to my page!",
+            skills: ["Math", "Html-Css", "JS", "Machine Learning"],
+            biopic: "../images/me.jpg",
+            display: {}
+          };
 
-bio.city = "HaNoi, VietNam";
-
-$("#main").append(bio.city);
-
-// formated lines
-
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
-var formattedEmail = HTMLemail.replace("%data%", bio.contact_info.email);
-
-var formattedMobile = HTMLmobile.replace("%data%", bio.contact_info.mobile);
-
-var formattedGithub = HTMLgithub.replace("%data%", bio.contact_info.github);
-
-var formattedPic = HTMLbioPic.replace("%data%", bio.picture_url);
-
-var formattedWellcome = HTMLwelcomeMsg.replace("%data%", bio.wellcom_msg);
-
-var formattedSkill = [];
-
-for(x in bio.skills) {
-  formattedSkill.push(HTMLskills.replace("%data%", bio.skills[x]));
-}
-
-
-$("#header").prepend(formattedSkill);
-
-$("#header").prepend(HTMLskillsStart);
-
-$("#header").prepend(formattedWellcome);
-
-$("#header").prepend(formattedPic);
-
-$("#header").prepend(formattedGithub);
-
-$("#header").prepend(formattedMobile);
-
-$("#header").prepend(formattedEmail);
-
-$("#header").prepend(formattedRole);
-
-$("#header").prepend(formattedName);
-
-// add education object
+/* 2. schools */
 var education = {
-                  name: "DHBK",
-                  degree: "Student",
-                  date: "2011",
-                  location: "HaNoi, VietNam",
-                  major: "Math"
-                }
+            schools: [
+              {
+                name: "Hanoi University of Science",
+                location: "HaNoi, VietNam",
+                degree: "engineer",
+                major: "",
+                date: "2012 - 2017",
+                url: "http://hust.edu.vn/web/vi/home"
+              }
+            ],
+            onlineCoures: [
+              {
+                title: "Machine Learning Foundations: A Case Study Approach",
+                school: "University of Washington",
+                date: "March 14 – May 2",
+                url: "https://www.coursera.org/learn/ml-foundations/home/welcome",
+              },
+              {
+                title: "Machine Learning: Regression",
+                school: "University of Washington",
+                date: "March 21 – May 9",
+                url: "https://www.coursera.org/learn/ml-regression/home/welcome",
+              },
+              {
+                title: "Machine Learning: Classification",
+                school: "University of Washington",
+                date: "May 30 – July 25",
+                url: "https://www.coursera.org/learn/ml-classification/home/welcome",
+              },
+            ],
+            display: {}
+          };
 
-// add work object
+
+/* 3. work */
 var work = {
-              employer: "Developer",
-              title: "Front-End",
-              date: "2015",
-              location: "HaNoi, VietNam",
-              desc: "front end developer."
-            }
-work["position"]="Monday";
-// change to formatted and add to html
-var formattedWorkEmployer = HTMLworkEmployer.replace("%data", work.employer);
+            jobs: [
+              {
+                employer: "JS developer",
+                title: "Chrome extension developer",
+                location: "HaNoi, VietNam",
+                date: "in progress",
+                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa."
+              },
+            ],
+            display: {}
+          };
 
-var formattedWorkTitle = HTMLworkTitle.replace("%data", work.title);
 
-var formattedWorkDate = HTMLworkDates.replace("%data", work.date);
-
-var formattedWorkLocation = HTMLworkLocation.replace("%data", work.location);
-
-var formattedWorkDesc = HTMLworkDescription.replace("%data", work.desc);
-
-var formattedSchoolName = HTMLschoolName.replace("%data", education.name);
-
-var formattedSchoolDegree = HTMLschoolDegree.replace("%data", education.degree);
-
-var formattedSchoolDate = HTMLschoolDates.replace("%data", education.date);
-
-var formattedSchoolLocation = HTMLschoolLocation.replace("%data", education.location);
-
-var formatedSchollMajor = HTMLschoolMajor.replace("%data", education.major);
-
-// append to work position and education position
-$("#main").append(work["position"]);
-
-$("#main").append(education.name);
+/* 4. Project */
+var projects = {
+            projects: [
+              {
+                title: "portfilio-udacity",
+                date: "August 24 - August 25",
+                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa.",
+                images: "http://placekitten.com/300/300",
+              },
+              {
+                title: "frontend-nanodegree-resume",
+                date: "in progress",
+                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa.",
+                images: "http://placekitten.com/300/240",
+              }
+            ],
+            display: {}
+          };
