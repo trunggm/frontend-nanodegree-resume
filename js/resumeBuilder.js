9c9cdac9527e6245fc76bffc3132c9ce8a7eea86
trunggm <trung.gm.vn@gmail.com>
2016-09-05 16:05:48 +0700

    fix required and customize my site

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,5 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+  Add my information to my site
  */
 var header, workExperience, projectView, educationView, mapDiv, letsConnect;
 header = $('#header');
@@ -40,9 +40,7 @@ function displayBio() {
 
   // display to index.html
   // display header name - role
-  header.prepend(role);
-  header.prepend(name);
-
+  header.prepend(name, role);
 
   for (var key in this.contacts) {
     if (this.contacts.hasOwnProperty(key)) {
@@ -50,20 +48,18 @@ function displayBio() {
       contactsItem = contactsItem.replace('%data%', this.contacts[key]);
       $('#topContacts').append(contactsItem);
       $('#footerContacts').append(contactsItem);
-    }
+    };
   }
 
   // display picture, welcomeMessage, skill
-  header.append(biopic);
-  header.append(welcomeMessage);
-  header.append(HTMLskillsStart);
+  header.append(biopic, welcomeMessage, HTMLskillsStart);
   if(bio.skills.length>0) {
     bio.skills.forEach(function (item, index) {
       var skillItem = HTMLskills.replace('%data%', item);
       $('#skills').append(skillItem);
     });
   }
-}
+};
 
 bio.display();
 
@@ -74,8 +70,8 @@ var education = {
                 name: "Hanoi University of Science",
                 location: "HaNoi, VietNam",
                 degree: "engineer",
-                major: "Applied Mathematics",
-                date: "2012 - 2017",
+                majors: "Applied Mathematics",
+                dates: "2012 - 2017",
                 url: "http://hust.edu.vn/web/vi/home"
               }
             ],
@@ -83,66 +79,62 @@ var education = {
               {
                 title: "Machine Learning Foundations: A Case Study Approach",
                 school: "University of Washington",
-                date: "March 14 – May 2",
+                dates: "March 14 – May 2",
                 url: "https://www.coursera.org/learn/ml-foundations/home/welcome",
               },
               {
                 title: "Machine Learning: Regression",
                 school: "University of Washington",
-                date: "March 21 – May 9",
+                dates: "March 21 – May 9",
                 url: "https://www.coursera.org/learn/ml-regression/home/welcome",
               },
               {
                 title: "Machine Learning: Classification",
                 school: "University of Washington",
-                date: "May 30 – July 25",
+                dates: "May 30 – July 25",
                 url: "https://www.coursera.org/learn/ml-classification/home/welcome",
               },
             ],
-            display: function () {
-              // display schools
-              if(this.schools.length>0) {
-                educationView.append(HTMLschoolStart);
-                var educationEntry = $('.education-entry');
-                this.schools.forEach(function (item, index) {
-                  var schoolName = HTMLschoolName.replace('%data%', item.name);
-                  schoolName = schoolName.replace('#', item.url);
-                  var schoolDegree = HTMLschoolDegree.replace('%data%', item.degree);
-                  var schoolDates = HTMLschoolDates.replace('%data%', item.date);
-                  var schoolLocation = HTMLschoolLocation.replace('%data%', item.location);
-                  var schoolMajor = HTMLschoolMajor.replace('%data%', item.major);
-
-                  // append to education-entry
-                  educationEntry.append(schoolName + schoolDegree);
-                  educationEntry.append(schoolDates);
-                  educationEntry.append(schoolLocation);
-                  educationEntry.append(schoolMajor);
-                });
-              }
-
-              // display onlineCoures
-              if(this.onlineCoures.length>0) {
-                educationView.append(HTMLonlineClasses);
-                this.onlineCoures.forEach(function (item, index) {
-                  educationView.append(HTMLschoolStart);
-                  var lastEntry = $('.education-entry:last');
-                  var itemTitle = HTMLonlineTitle.replace('%data%', item.title);
-                  itemTitle = itemTitle.replace('#', item.url);
-                  var itemSchool = HTMLonlineSchool.replace('%data%', item.school);
-                  var itemDate = HTMLonlineDates.replace('%data%', item.date);
-                  var itemLink = HTMLonlineURL.replace('%data%', item.url);
-                  itemLink = itemLink.replace('#', item.url);
-
-                  // append to online course
-                  lastEntry.append(itemTitle + itemSchool);
-                  lastEntry.append(itemDate);
-                  lastEntry.append(itemLink);
-                });
-              }
-            },
+            display: displayEdu,
           };
 
 /* display education*/
+function displayEdu() {
+  // display schools
+  if(this.schools.length>0) {
+    educationView.append(HTMLschoolStart);
+    var educationEntry = $('.education-entry');
+    this.schools.forEach(function (item, index) {
+      var schoolName = HTMLschoolName.replace('%data%', item.name);
+      schoolName = schoolName.replace('#', item.url);
+      var schoolDegree = HTMLschoolDegree.replace('%data%', item.degree);
+      var schoolDates = HTMLschoolDates.replace('%data%', item.dates);
+      var schoolLocation = HTMLschoolLocation.replace('%data%', item.location);
+      var schoolMajors = HTMLschoolMajor.replace('%data%', item.majors);
+
+      // append to education-entry
+      educationEntry.append(schoolName + schoolDegree, schoolDates, schoolLocation, schoolMajors);
+    });
+  }
+
+  // display onlineCoures
+  if(this.onlineCoures.length>0) {
+    educationView.append(HTMLonlineClasses);
+    this.onlineCoures.forEach(function (item, index) {
+      educationView.append(HTMLschoolStart);
+      var lastEntry = $('.education-entry:last');
+      var itemTitle = HTMLonlineTitle.replace('%data%', item.title);
+      itemTitle = itemTitle.replace('#', item.url);
+      var itemSchool = HTMLonlineSchool.replace('%data%', item.school);
+      var itemDates = HTMLonlineDates.replace('%data%', item.dates);
+      var itemLink = HTMLonlineURL.replace('%data%', item.url);
+      itemLink = itemLink.replace('#', item.url);
+
+      // append to online course
+      lastEntry.append(itemTitle + itemSchool, itemDates, itemLink);
+    });
+  }
+};
 
 
 education.display();
@@ -155,39 +147,38 @@ var work = {
                 employer: "JS developer",
                 title: "Chrome extension developer",
                 location: "HaNoi, VietNam",
-                date: "December 28, 2015 - Future",
+                dates: "December 28, 2015 - Future",
                 description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa."
               },
-
               {
                 employer: "Seller",
                 title: "Castrol Seller",
                 location: "HaNoi, VietNam",
-                date: "December 1, 2013 - 2014",
+                dates: "December 1, 2013 - 2014",
                 description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa."
               },
             ],
-            display: function () {
-              if (this.jobs.length>0) {
-                this.jobs.forEach(function (item, index) {
-                  workExperience.append(HTMLworkStart);
-                  var workLastEntry = $('.work-entry:last');
-                  var workEmployer = HTMLworkEmployer.replace('%data%', item.employer);
-                  var workTitle = HTMLworkTitle.replace('%data%', item.title);
-                  var workDate = HTMLworkDates.replace('%data%', item.date);
-                  var workLocation = HTMLworkLocation.replace('%data%', item.location);
-                  var workDesc = HTMLworkDescription.replace('%data%', item.description);
-
-                  workLastEntry.append(workEmployer + workTitle);
-                  workLastEntry.append(workDate);
-                  workLastEntry.append(workLocation);
-                  workLastEntry.append(workDesc);
-                })
-              }
-            },
+            display: displayWork,
           };
 
 // display work
+function displayWork () {
+  if (this.jobs.length>0) {
+    this.jobs.forEach(function (item, index) {
+      workExperience.append(HTMLworkStart);
+      var workLastEntry = $('.work-entry:last');
+      var workEmployer = HTMLworkEmployer.replace('%data%', item.employer);
+      var workTitle = HTMLworkTitle.replace('%data%', item.title);
+      var workDates = HTMLworkDates.replace('%data%', item.dates);
+      var workLocation = HTMLworkLocation.replace('%data%', item.location);
+      var workDesc = HTMLworkDescription.replace('%data%', item.description);
+
+      workLastEntry.append(workEmployer + workTitle, workDates, workLocation, workDesc);
+    });
+  }
+};
+
+
 work.display();
 
 /* 4. Project */
@@ -195,7 +186,7 @@ var projects = {
             projects: [
               {
                 title: "portfilio-udacity",
-                date: "August 24 - August 25",
+                dates: "August 24 - August 25",
                 description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa.",
                 images: [
                   "http://placekitten.com/300/300"
@@ -203,7 +194,7 @@ var projects = {
               },
               {
                 title: "frontend-nanodegree-resume",
-                date: "in progress",
+                dates: "in progress",
                 description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, corrupti ea iure libero officia autem veritatis architecto accusamus? Debitis, ipsa.",
                 images: [
                   "http://placekitten.com/300/240",
@@ -212,30 +203,31 @@ var projects = {
                 ]
               }
             ],
-            display: function () {
-              if (this.projects.length>0) {
-                this.projects.forEach( function (item, index) {
-                  projectView.append(HTMLprojectStart);
-                  var projectLastEntry = $('.project-entry:last');
-                  var projectTitle = HTMLprojectTitle.replace('%data%', item.title);
-                  var projectDate = HTMLprojectDates.replace('%data%', item.date);
-                  var projectDesc = HTMLprojectDescription.replace('%data%', item.description);
-
-                  projectLastEntry.append(projectTitle);
-                  projectLastEntry.append(projectDate);
-                  projectLastEntry.append(projectDesc);
-                  if (item.images.length>0) {
-                    item.images.forEach( function (itemX, index) {
-                      var projectImg = HTMLprojectImage.replace('%data%', itemX);
-                      projectLastEntry.append(projectImg);
-                    });
-                  }
-                })
-              }
-            }
+            display: displayProject,
           };
 
 // display project
+function displayProject () {
+  if (this.projects.length>0) {
+    this.projects.forEach( function (item, index) {
+      projectView.append(HTMLprojectStart);
+      var projectLastEntry = $('.project-entry:last');
+      var projectTitle = HTMLprojectTitle.replace('%data%', item.title);
+      var projectDates = HTMLprojectDates.replace('%data%', item.dates);
+      var projectDesc = HTMLprojectDescription.replace('%data%', item.description);
+
+      projectLastEntry.append(projectTitle, projectDates, projectDesc);
+
+      if (item.images.length>0) {
+        item.images.forEach( function (itemX, index) {
+          var projectImg = HTMLprojectImage.replace('%data%', itemX);
+          projectLastEntry.append(projectImg);
+        });
+      }
+    })
+  }
+};
+
 projects.display();
 
 /* display map */
